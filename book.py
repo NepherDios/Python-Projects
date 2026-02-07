e81e05c6dd96a8bc7302057d62a20ef092beb8af
NepherDios <rui.moreira.programmer@gmail.com>
2026-02-07 23:36:26 +0000

    Refactored Code

--- book.py
+++ book.py
@@ -5,28 +5,37 @@ class Book:
         self.pages = pages
         
     def describe(self):
-        print(f"""\tBook Description:
+        return f"""\tBook Description:
             Title: {self.title}
             Author: {self.author}
             Pages: {self.pages}
-        """)
+        """
         
 class LibraryBook(Book):
+    def __init__(self, title: str, author: str, pages: int):
+        super().__init__(title, author, pages)
+        self._borrowed = False
+    
     def borrow(self):
-        self.borrowed = True
+        if(self.is_borrowed()):
+            raise Exception("Book already borrowed!")
+        
+        self._borrowed = True
         
         
     def return_book(self):
-        self.borrowed = False
+        self._borrowed = False
         
-    def book_state(self):
-        return self.borrowed
+    def is_borrowed(self):
+        return self._borrowed
         
 
 book_1 = Book("Glaze 2", "Admin", 30)
 book_2 = Book("Glaze 1", "Admin", 27)
 book_3 = LibraryBook("The Blast", "Admin", 30)
 
-print(f"Book 1: {book_1.describe()}")
-print(f"Book 2: {book_2.describe()}")
-print(f"Book 3: {book_3.describe()}")
+print("Book 1: \n", book_1.describe())
+print("Book 2: \n", book_2.describe())
+print("Book 3: \n", book_3.describe())
+
+print(f"Book 3 state: {book_3.is_borrowed()}")
