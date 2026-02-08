a2dc829272b9b14d20e7feb8735c29503140771d
Rui Moreira <159066723+NepherDios@users.noreply.github.com>
2026-02-08 00:12:35 +0000

    Delete nameListFilter.py

--- nameListFilter.py
+++
@@ -1,7 +0,0 @@
-def filterNameBySize(namesList):
-	for i in namesList:
-		if len(i) > 4:
-			print(f"{i}")
-
-namesList = ["Moreira", "Derick", "Pietro", "Tay", "Price"]
-filterNameBySize(namesList)
